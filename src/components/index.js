4e92a9965897ca8e84d725abe694d32132a1e235
Frank <mirandofrank@gmail.com>
2025-05-04 20:02:54 -0700

    Removed testimonials

--- src/components/index.js
+++ src/components/index.js
@@ -5,7 +5,7 @@ import About from './About';
 import Tech from './Tech';
 import Experience from './Experience';
 import Works from './Works';
-import Feedbacks from './Feedbacks';
+// import Feedbacks from './Feedbacks';
 import Contact from './Contact';
 
 export {
@@ -15,10 +15,9 @@ export {
   Tech,
   Experience,
   Works,
-  Feedbacks,
   Contact,
-  EarthCanvas, 
-  BallCanvas, 
-  ComputersCanvas, 
+  EarthCanvas,
+  BallCanvas,
+  ComputersCanvas,
   StarsCanvas
 }
